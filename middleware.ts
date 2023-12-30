c74c63a8f3e33fa90563726640b1c9869aad49dc
pratikn987 <pratiknikat01@gmail.com>
2023-12-31 03:00:35 +0530

    fgt

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhook",
-    "/question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
-  ],
+  publicRoutes: ["/"],
   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
